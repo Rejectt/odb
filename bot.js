0ccd1cfc0514a7ff4617a105888687497907a8f3
Rejectt <45071435+Rejectt@users.noreply.github.com>
2018-12-18 07:30:17 -0800

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+
+login.bot('NTI0NjA1NTk3Njk0NDI3MTg3.Dvqi6A.l6VUgGvCih3l3h8g5W_aaD-dFRo')
